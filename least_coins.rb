57432c24b84552ac30b4278f1f51900d62534325
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 21:19:35 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,29 @@
 #write out your code here
 
 def least_coins(cents)
+  puts coins = {"quarters" => 0, "dimes" => 10, "nickels" => 0, "pennies" => 0}
+ 
 
-#Code your answer here!
 
+while cents >= 25
+coins["quarters"] += 1
+cents -= 25
 end
+
+while cents >= 10
+coins["dimes"] += 1
+cents -= 10
+end
+ 
+while cents >= 5
+coins["nickels"] += 1
+cents -= 5
+end
+
+while cents >= 1
+coins["pennies"] += 1
+cents -= 1
+end
+end 
+
+
---
+++ nested_data.rb
@@ -0,0 +1,14 @@
+
+      # tops = ["blouses", "t-shirts", "jackets"]
+      # bottoms = ["jeans", "skirts", "shorts"]
+      # footwear =  ["sandals", "sneakers", "heels"]
+      # clothes = [tops, bottoms, footwear]
+      # footwear << ["birkenstocks"]
+   
+      # puts clothes
+closet = {
+    "tops" => ["rain jacket", "cardigan", "blazer"], 
+    "bottoms" => ["jeans", "skirt", "capris"], 
+    "footwear" => ["sneakers", "heels", "flats"]
+}
+ puts closet["tops"] << "tank top"
